8b45ea6c739e5ff107cd677d59ff5cea08475c4a
varun Chauhan <varun.chauhan.it@gmail.com>
2022-05-19 10:07:51 +0530

    removed this file

--- Adobe Acrobat DC-V.1.bat
+++
@@ -1,6 +0,0 @@
-echo samtech#123 | runas /user:samadmin cmd
-erase C:\Program Files (x86)\Adobe\Acrobat DC\Acrobat\amtlib.dll
-
-cd C:\Program Files (x86)\Adobe\Acrobat DC
-
-copy \amtlib.dll C:\Program Files (x86)\Adobe\Acrobat DC\Acrobat\amtlib.dll
