052125e455710425f02c7e55cbba42fdbccf42fb
Varun Chauhan <varunachauhanom@gmail.com>
2023-05-24 13:25:27 +0530

    add command for start application

--- Download & Install Application.bat
+++ Download & Install Application.bat
@@ -1,18 +1,23 @@
 @echo off
-CD %userprofile%\Downloads\
-curl https://test.samtechdatasys.com/software-installtion/InScribeSetup.msi -O
-curl https://test.samtechdatasys.com/software-installtion/spark_2_8_0.msi -O
-curl https://test.samtechdatasys.com/software-installtion/Thunderbird.exe -O
-echo Please wait, Inscribe installation is in progress.
-msiexec.exe /i "%userprofile%\Downloads\InScribeSetup.msi" /qb!
-msg "%username%" Inscribe sucessfully installed.
-echo Inscribe sucessfully installed.
-echo Please wait, Spark installation is in progress.
-msiexec.exe /i "%userprofile%\Downloads\spark_2_8_0.msi" 
-echo Spark sucessfully installed
-msg "%username%" Spark sucessfully installed
-set /p answer= Do you want install Thunderbird ,please type(Y/N)?
-if /i "%answer:~,1%" EQU "Y" msiexec.exe /i "%userprofile%\Downloads\Thunderbird.msi" /qb!
+call CD %userprofile%\Downloads\
+call curl https://test.samtechdatasys.com/soft/Setup/InScribeSetup.msi -O
+call curl https://test.samtechdatasys.com/soft/Setup/spark_3_0_0.msi -O
+call curl https://test.samtechdatasys.com/soft/Setup/Thunderbird.exe -O
+call curl https://test.samtechdatasys.com/soft/Setup/spark.properties -O
+call echo Please wait, Inscribe installation is in progress.
+call msiexec.exe /i "%userprofile%\Downloads\InScribeSetup.msi" /qn
+call echo Inscribe sucessfully installed.
+call START "" "C:\Program Files (x86)\InScribe 7\InScribe.exe"
+call echo Please wait, Spark installation is in progress.
+call msiexec.exe /i "%userprofile%\Downloads\spark_3_0_0.msi" /qn
+call echo Spark sucessfully installed
+call START "" "C:\Program Files (x86)\Spark\Spark.exe"
+call taskkill /im Spark.exe /t /f
+call xcopy /s %userprofile%\Downloads\spark.properties %appdata%\Spark /Y
+call START "" "C:\Program Files (x86)\Spark\Spark.exe"
+call set /p answer= Do you want install Thunderbird ,please type(Y/N)?
+if /i "%answer:~,1%" EQU "Y" PowerShell.exe Start-Process -wait -FilePath "%userprofile%\Downloads\Thunderbird.exe" -ArgumentList '/S','/v','/qn'
 if /i "%answer:~,1%" EQU "N" exit /b
-msg "%username%" Thunderbird sucessfully installed
-
+call START "" "C:\Program Files\Mozilla Thunderbird\thunderbird.exe"
+call echo Thunderbird sucessfully installed.
+call msg "%username%" Software sucessfully installed
