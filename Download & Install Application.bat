66db2aabd138833e0d5422d935b3b81528be9446
Varun Chauhan <varunachauhanom@gmail.com>
2023-05-25 10:02:12 +0530

    add regional formate command

--- Download & Install Application.bat
+++ Download & Install Application.bat
@@ -7,6 +7,7 @@ call curl https://test.samtechdatasys.com/soft/Setup/spark.properties -O
 call echo Please wait, Inscribe installation is in progress.
 call msiexec.exe /i "%userprofile%\Downloads\InScribeSetup.msi" /qn
 call echo Inscribe sucessfully installed.
+call PowerShell.exe Set-Culture en-us
 call START "" "C:\Program Files (x86)\InScribe 7\InScribe.exe"
 call echo Please wait, Spark installation is in progress.
 call msiexec.exe /i "%userprofile%\Downloads\spark_3_0_0.msi" /qn
@@ -15,6 +16,7 @@ call START "" "C:\Program Files (x86)\Spark\Spark.exe"
 call taskkill /im Spark.exe /t /f
 call xcopy /s %userprofile%\Downloads\spark.properties %appdata%\Spark /Y
 call START "" "C:\Program Files (x86)\Spark\Spark.exe"
+Call start outlook.exe
 call set /p answer= Do you want install Thunderbird ,please type(Y/N)?
 if /i "%answer:~,1%" EQU "Y" PowerShell.exe Start-Process -wait -FilePath "%userprofile%\Downloads\Thunderbird.exe" -ArgumentList '/S','/v','/qn'
 if /i "%answer:~,1%" EQU "N" exit /b
